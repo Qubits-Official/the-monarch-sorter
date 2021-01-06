86b36244d12b72ffa47020f6c0e9414a89c29c9d
Marc Peejay Viernes <mpvviernes@gmail.com>
2021-01-06 13:19:57 +0800

    Add the program flow as a comment.

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+""" Program Flow
+    1) Get the method of sorting (a or b).
+    2) Get the number of monarchs to sort.
+    3) Get the list of monarchs one line at a time.
+    4) Sort the list according to the chosen method
+    5) Print the results.
+"""
+
 def main():
     pass
 
